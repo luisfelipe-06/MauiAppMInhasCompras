3502270d291ba8d11e058f1210bdb9621a423009
skyfall <luisfelip896@gmail.com>
2026-03-15 12:38:06 -0300

    Mudanças: Add ObservableCollection, Button Remove, Change Views da Lista.

--- MauiAppMInhasCompras/Models/Produto.cs
+++ MauiAppMInhasCompras/Models/Produto.cs
@@ -9,5 +9,6 @@ namespace MauiAppMInhasCompras.Models
         public string Descricao { get; set; }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
+        public double Total { get => Quantidade * Preco; }
     }
 }
