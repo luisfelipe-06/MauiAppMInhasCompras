3a7dee65d927d8072c1371db0aa02d61424af51f
skyfall <luisfelip896@gmail.com>
2026-03-28 17:03:45 -0300

    Validação da inserção de produto e correção dos produtos duplicados na tela inicial

--- MauiAppMInhasCompras/Models/Produto.cs
+++ MauiAppMInhasCompras/Models/Produto.cs
@@ -4,11 +4,46 @@ namespace MauiAppMInhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao { 
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("A descrição do produto é obrigatória.");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade {
+            get => _quantidade;
+            set
+            {
+              if (value <= 0)
+                {
+                    throw new Exception("A quantidade deve ser maior que zero.");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco { 
+            get => _preco;
+            set
+            {                 
+                if (value < 0)
+                {
+                    throw new Exception("O preço não pode ser negativo.");
+                }
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
